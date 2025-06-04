16519a8a966b51a7bc5529dd16c7e8873ab10d26
adhamjon0770 <isagi3572@gmail.com>
2025-06-04 18:12:39 +0500

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import img1 from '.assets/premium_photo-1664299631876-f143dc691c4d.jpg'
 function App() {
   return (
    <>
@@ -8,6 +8,7 @@ function App() {
    <h2> hello world</h2>
    </h1>
 <h4>hello ...</h4>
+<img src={img1} alt="" />
    </>
   )
 }
