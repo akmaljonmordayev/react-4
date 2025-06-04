1b05000eaad50b66c22b8e8a1126e3f4c25c0944
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-06-04 18:26:52 +0500

    ol added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './App.css'
-import img1 from './assets/premium_photo-1664299631876-f143dc691c4d.jpg'
+import "./App.css";
+import img1 from "./assets/premium_photo-1664299631876-f143dc691c4d.jpg";
 function App() {
   return (
-   <>
-   <h1>
-    Lorem ipsum dolor sit.
-   <h2> hello world</h2>
-   </h1>
-<h4>hello ...</h4>
-<img src={img1} alt="" />
-   </>
-  )
+    <>
+      <h1>
+        Lorem ipsum dolor sit.
+        <h2> hello world</h2>
+      </h1>
+      <h4>hello ...</h4>
+      <img src={img1} alt="" />
+      <ol>
+        <li>olma</li>
+      </ol>
+    </>
+  );
 }
 
-export default App
+export default App;
